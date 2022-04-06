7079289ee589112375c7df95f658362f3e17c6a6
Jade <jaderichard@live.fr>
2022-04-06 09:12:58 +0100

    auth

--- laravel-relations-exercice3/routes/web.php
+++ laravel-relations-exercice3/routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TeamController;
-use App\Models\Player;
-use App\Models\Role;
-use App\Models\Team;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,30 +14,11 @@ use App\Models\Team;
 */
 
 Route::get('/', function () {
-    $players = Player::all();
-    $teams = Team::all();
-    $noteam = Player::all()->where('team_id', '==', '5')->random(4);
-    $rteam = Player::all()->where('team_id', '!=', '5')->random(4);
-    $playerF = Player::all()->where('team_id', '!=', '5')->where('gender', '==', 'F')->random(5); 
-    $playerM = Player::all()->where('team_id', '!=', '5')->where('gender', '==', 'M')->random(5); 
-    $playerSpain = Player::all()->where('team_id', '==', '3')->where('country', '==', 'Spain'); 
-    $playerTaiwan = Player::all()->where('team_id', '==', '2')->where('country', '==', 'Taiwan'); 
-    return view('welcome', compact("players", "teams", "playerM", "playerF", "noteam", "rteam", "playerSpain", "playerTaiwan"));
+    return view('welcome');
 });
-// team
-Route::get('/back/teams', [TeamController::class, 'index'])->name('team.index');
-Route::get('/back/teams/create', [TeamController::class, 'create'])->name('team.create');
-Route::post('/back/teams/store', [TeamController::class, 'store'])->name('team.store');
-Route::get('/back/teams/{id}/read', [TeamController::class, 'read'])->name('team.read');
-Route::get('/back/teams/{id}/edit', [TeamController::class, 'edit'])->name('team.edit');
-Route::post('/back/teams/{id}/update', [TeamController::class, 'update'])->name('team.update');
-Route::post('/back/teams/{id}/destroy', [TeamController::class, "destroy"])->name('team.destroy');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/back/players', [PlayerController::class, 'index'])->name('player.index');
-Route::get('/back/players/create', [PlayerController::class, 'create'])->name('player.create');
-Route::post('/back/players/store', [PlayerController::class, 'store'])->name('player.store');
-Route::get('/back/players/{id}/read', [PlayerController::class, 'read'])->name('player.read');
-Route::get('/back/players/{id}/edit', [PlayerController::class, 'edit'])->name('player.edit');
-Route::post('/back/players/{id}/update', [PlayerController::class, 'update'])->name('player.update');
-Route::post('/back/players/{id}/destroy', [PlayerController::class, "destroy"])->name('player.destroy');
+require __DIR__.'/auth.php';
