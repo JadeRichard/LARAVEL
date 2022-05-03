9957efe10aa7e715919a43153f45e5918e16f63c
Jade <jaderichard@live.fr>
2022-05-03 10:01:23 +0100

    commit 2

---
+++ laravel-remediation/database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => "Admin",
+        ]);
+        DB::table('roles')->insert([
+            'name' => "Webmaster",
+        ]);
+        DB::table('roles')->insert([
+            'name' => "Role3",
+        ]);
+        DB::table('roles')->insert([
+            'name' => "Role4",
+        ]);
+    }
+}
